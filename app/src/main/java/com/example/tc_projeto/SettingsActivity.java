70e04858ee838096c4ed95e7f82b3698d7cc448f
Eduardo <eassunca@gmail.com>
2024-11-28 10:45:34 +0000

    definicoes

--- app/src/main/java/com/example/tc_projeto/SettingsActivity.java
+++ app/src/main/java/com/example/tc_projeto/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +18,23 @@ public class SettingsActivity extends AppCompatActivity {
         ImageButton homeButton = findViewById(R.id.button_home);
         ImageButton notificationsButton = findViewById(R.id.button_notifications);
         ImageButton settingsButton = findViewById(R.id.button_settings);
+        Switch noti = findViewById(R.id.notification_switch);
+        Switch incendio = findViewById(R.id.notification_switch3);
+        Switch inundacao = findViewById(R.id.notification_switch2);
+        Switch tempestade = findViewById(R.id.notification_switch4);
+
+        //Definições de Notificações
+
+        noti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
 
         // Barra de navegação
         homeButton.setOnClickListener(new View.OnClickListener() {
